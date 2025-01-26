5675caa1cda39c84944191161407f80d56a27210
hlyudzyk <valerijglud0@gmail.com>
2025-01-26 21:42:08 +0200

    SETUP-1: initialized empty spring boot app

---
+++ statrack/src/main/java/com/statrack/statrack/StatrackApplication.java
@@ -0,0 +1,13 @@
+package com.statrack.statrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatrackApplication.class, args);
+	}
+
+}
