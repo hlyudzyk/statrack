299e04714ffe220b1bebbd4b2066ccbee4db653c
hlyudzyk <valerijglud0@gmail.com>
2025-01-27 21:11:52 +0200

    FT-2: Added data layer and basic services

---
+++ statrack/src/main/java/com/statrack/statrack/services/UserRoleService.java
@@ -0,0 +1,37 @@
+package com.statrack.statrack.services;
+
+import com.statrack.statrack.security.user.Role;
+import com.statrack.statrack.security.user.User;
+import com.statrack.statrack.security.user.UserRepository;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+public class UserRoleService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserRoleService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<SimpleGrantedAuthority> getUserAuthorities(UUID userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("User not found"));
+        return user.getRole().getAuthorities();
+    }
+
+    @Transactional
+    public User updateUserRole(UUID userId, Role newRole) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("User not found"));
+        user.setRole(newRole);
+        return userRepository.save(user);
+    }
+}
---
+++ statrack/src/main/java/com/statrack/statrack/services/UserService.java
@@ -0,0 +1,33 @@
+package com.statrack.statrack.services;
+
+import com.statrack.statrack.security.user.User;
+import com.statrack.statrack.security.user.UserRepository;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(UUID id) {
+        return userRepository.findById(id);
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+
+    }
+    public void deleteUser(UUID id) {
+        userRepository.deleteById(id);
+    }
+}
